68f25f9183e23d4b453c8b7b1746537549de71e3
Daniel Kerr <lorddanp@aol.com>
2017-11-09 02:34:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,38 @@ function destructivelyAppendKitten(name) {
   kittens.push(`${name}`);
   return kittens;
 }
+
+function destructivelyPrependKitten (name){
+  kittens.unshift(`${name}`);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;    
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.splice(1,1);
+  return kittens;
+}
+
+function appendKitten(name){
+  var newKitten = [...kittens, name];
+  return newKitten;
+}
+
+function prependKitten(name){
+  var newKitten = [name, ...kittens];
+  return newKitten;
+}
+
+function removeLastKitten(){
+  var newKitten = kittens.slice(1, kittens.length-1);
+  return newKitten;
+}
+
+function removeFirstKitten() {
+  var newKitten = kittens.slice(2, kittens.length)
+  return newKitten;
+}
